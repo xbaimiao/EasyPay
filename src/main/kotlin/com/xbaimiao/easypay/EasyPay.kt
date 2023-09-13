4282df20c81abec98306ff9270d6283dbaabee24
小白 <3104026189@qq.com>
2023-09-13 22:58:39 +0800

    del

--- src/main/kotlin/com/xbaimiao/easypay/EasyPay.kt
+++ src/main/kotlin/com/xbaimiao/easypay/EasyPay.kt
@@ -1,9 +1,6 @@
 package com.xbaimiao.easypay
 
 import com.xbaimiao.easylib.EasyPlugin
-import com.xbaimiao.easylib.skedule.SynchronizationContext
-import com.xbaimiao.easylib.skedule.schedule
-import com.xbaimiao.easylib.util.info
 import com.xbaimiao.easylib.util.warn
 import com.xbaimiao.easypay.api.CommandItem
 import com.xbaimiao.easypay.api.ItemProvider
@@ -17,27 +14,6 @@ class EasyPay : EasyPlugin() {
 
     override fun enable() {
         saveDefaultConfig()
-
-        schedule {
-            val startTime = System.currentTimeMillis()
-            println(Thread.currentThread().name)
-            val a = runAsync {
-                Thread.sleep(1000)
-                println(Thread.currentThread().name)
-                "返回1"
-            }
-            val b = runAsync {
-                Thread.sleep(2000)
-                println(Thread.currentThread().name)
-                "返回2"
-            }
-
-            switchContext(SynchronizationContext.SYNC)
-            println(Thread.currentThread().name)
-            info("${a.await()} ${b.await()}")
-            println(Thread.currentThread().name)
-            info("耗时: ${System.currentTimeMillis() - startTime} ms")
-        }
 
         if (config.getString("alipay.appid") == "appid") {
             warn("未配置支付宝Service 不加载")
