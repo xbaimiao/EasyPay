7262854134551bbe4c7c2230a191b5df001d2c4f
小白 <3104026189@qq.com>
2023-10-14 11:43:42 +0800

    累计充值 update

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ easylib {
 }
 
 group = "com.xbaimiao.easypay"
-version = "1.0.6"
+version = "1.0.7"
 
 repositories {
     mavenLocal()
--- src/main/kotlin/com/xbaimiao/easypay/reward/RewardHandle.kt
+++ src/main/kotlin/com/xbaimiao/easypay/reward/RewardHandle.kt
@@ -2,10 +2,12 @@ package com.xbaimiao.easypay.reward
 
 import com.xbaimiao.easylib.chat.BuiltInConfiguration
 import com.xbaimiao.easylib.chat.colored
+import com.xbaimiao.easylib.skedule.schedule
 import com.xbaimiao.easylib.ui.Basic
 import com.xbaimiao.easylib.ui.Variable
 import com.xbaimiao.easylib.ui.convertItem
 import com.xbaimiao.easylib.util.warn
+import com.xbaimiao.easypay.database.Database
 import org.bukkit.entity.Player
 
 object RewardHandle {
@@ -17,40 +19,54 @@ object RewardHandle {
     }
 
     fun open(player: Player) {
-        val title = configuration.getString("title", " ")!!.colored()
-        val sort = configuration.getStringList("sort").map { it.toCharArray().toList() }
+        schedule {
+            val title = configuration.getString("title", " ")!!.colored()
+            val sort = configuration.getStringList("sort").map { it.toCharArray().toList() }
 
-        val basic = Basic(player, title)
-        basic.rows(sort.size)
-        basic.slots.addAll(sort)
-        basic.onDrag { it.isCancelled = true }
-        basic.onClick { it.isCancelled = true }
+            val basic = Basic(player, title)
+            basic.rows(sort.size)
+            basic.slots.addAll(sort)
+            basic.onDrag { it.isCancelled = true }
+            basic.onClick { it.isCancelled = true }
 
-        val section = configuration.getConfigurationSection("items")
 
-        if (section != null) {
-            for (key in section.getKeys(false)) {
-                if (key.length > 1) {
-                    warn("buildMenu: $key is not a char")
-                    continue
-                }
-                val internalName = section.getString("$key.internal-name")
-                val price = section.getDouble("$key.price", 0.0)
-                val commands = section.getStringList("$key.commands")
-                val reward = internalName?.let { Reward(it, price, commands) }
-                if (reward != null) {
-                    basic.onClick(key[0]) {
-                        if (reward.sendTo(player)) {
-                            open(player)
+            async {
+                val section = configuration.getConfigurationSection("items")
+                val varTotal = Variable("%total%", Database.inst().getAllOrder(player.name).sumOf { it.price }.toString())
+
+                if (section != null) {
+                    for (key in section.getKeys(false)) {
+                        if (key.length > 1) {
+                            warn("buildMenu: $key is not a char")
+                            continue
                         }
+                        val internalName = section.getString("$key.internal-name")
+                        val price = section.getDouble("$key.price", 0.0)
+                        val commands = section.getStringList("$key.commands")
+
+                        val currentVars = arrayListOf(varTotal)
+
+                        val reward = internalName?.let { Reward(it, price, commands) }
+                        if (reward != null) {
+                            basic.onClick(key[0]) {
+                                schedule {
+                                    val result = async { reward.sendTo(player) }
+                                    if (result) {
+                                        open(player)
+                                    }
+                                }
+                            }
+                        }
+                        if (reward != null) {
+                            currentVars.add(Variable("%state%", reward.preSendState(player)))
+                        }
+                        basic.set(key[0], section.convertItem(player, key, currentVars))
                     }
                 }
-                val vars = if (reward != null) listOf(Variable("%state%", reward.preSendState(player))) else emptyList()
-                basic.set(key[0], section.convertItem(player, key, vars))
             }
-        }
 
-        basic.openAsync()
+            basic.openAsync()
+        }
     }
 
 }
