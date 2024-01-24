a59bb647925e7cf2f37aacaabf55c9d5ab67c885
killerprojecte <admin@fastmcmirror.org>
2024-01-24 14:19:07 +0800

    EvalEx2: streq function

--- src/main/kotlin/com/xbaimiao/easypay/functions/EvalEx2ConditionFunction.kt
+++ src/main/kotlin/com/xbaimiao/easypay/functions/EvalEx2ConditionFunction.kt
@@ -1,6 +1,8 @@
 package com.xbaimiao.easypay.functions
 
+import com.udojava.evalex.AbstractLazyFunction
 import com.udojava.evalex.Expression
+import com.udojava.evalex.Expression.LazyNumber
 import com.udojava.evalex.ExpressionSettings
 import com.xbaimiao.easypay.parameters.PlayerParameter
 import com.xbaimiao.easypay.results.CancellableResult
@@ -19,6 +21,7 @@ class EvalEx2ConditionFunction : FastFunction {
     override fun call(parameter: FunctionParameter?): FunctionResult {
         val playerParameter = parameter!! as PlayerParameter
         val expression = Expression(playerParameter.string, settings)
+        expression.addLazyFunction(streqFunction)
         val expressionValue = expression.eval()
         if (expressionValue.compareTo(BigDecimal.ONE) == 0) {
             return CancellableResult(true)
@@ -28,5 +31,36 @@ class EvalEx2ConditionFunction : FastFunction {
 
     override fun getName(): String {
         return "if"
+    }
+
+    class STREQFunction : AbstractLazyFunction("STREQ", 2) {
+        private val falseResult = object : LazyNumber {
+            override fun eval(): BigDecimal {
+                return BigDecimal.ZERO
+            }
+
+            override fun getString(): String {
+                return "0"
+            }
+        }
+
+        private val trueResult = object : LazyNumber {
+            override fun eval(): BigDecimal {
+                return BigDecimal.ONE
+            }
+
+            override fun getString(): String {
+                return "1"
+            }
+
+        }
+
+        override fun lazyEval(lazyParams: MutableList<LazyNumber>): LazyNumber {
+            return if (lazyParams[0].string.equals(lazyParams[1].string)) trueResult else falseResult
+        }
+    }
+
+    companion object {
+        private val streqFunction = STREQFunction()
     }
 }
